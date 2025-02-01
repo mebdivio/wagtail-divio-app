27079d05a1ee3d6b51d3c689345b19ffa0c9a38d
Mebrahtu Tesfazghi <mebrahtu.tesfazghi@divio.ch>
2025-02-01 03:53:08 +0100

    Initial commit: Wagtail project setup

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!bwd=(*r88uj+#=o6xn#a0s^%0&9s)60=2)ei=_ld72-$m%i(#"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
